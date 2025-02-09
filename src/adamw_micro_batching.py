30a7cdea7fe83dd161d931b913a4b873dd434875
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-08 20:21:35 -0800

    lanczos micro-batch update

--- src/adamw_micro_batching.py
+++ src/adamw_micro_batching.py
@@ -70,13 +70,142 @@ def gaussian(x, x0, sigma_squared):
                   (2.0 * sigma_squared)) / np.sqrt(2 * np.pi * sigma_squared)
 
 
-class LanczosAlgorithm:
+# in the basic Lanczos call, the grad is computed once for the batch
+# HVP is computed num_iteration times => O(num_itrs + 1) for forward-backward passes per batch
+
+# if you cannot cache grads, the HVP computation is O(num_itrs * batch + num_itrs) for forward-backward passes per batch
+
+# can you cache this by adding to a list, and placing it off GPU
+# this line will need to be done by brining one micro-batch computation graph in at a time: grad_v_prod = torch.sum(self.grad_params * v)
+class MicrobatchLanczos:
     def __init__(self, num_iterations=20, tol=1e-6):
+        self.num_iterations = num_iterations
+        self.tol = tol
+        self.init_vector = None
+
+    def hvp(self, model, loss_fn, train_dataset, params, v, preconditioner=None):
+        hvp = None
+
+        # if preconditioner is not None, we need to compute P^(-1/2) * v
+        if preconditioner is not None:
+            v = v / preconditioner.sqrt()
+
+        itr = 0
+        total_items = 1000
+        for (X, y) in iterate_dataset(train_dataset, 100):
+            outputs = model(X.cuda())
+            loss = loss_fn(outputs, y.cuda()) / total_items
+
+            grad_params = torch.autograd.grad(loss, params, create_graph=True)
+            grad_params = torch.cat([g.flatten() for g in grad_params])
+
+            grad_v_prod = torch.sum(grad_params * v)
+            _hvp = torch.autograd.grad(grad_v_prod, params, retain_graph=True)
+            _hvp = torch.cat([g.flatten() for g in _hvp])
+
+            if hvp is None:
+                hvp = _hvp.detach()
+            else:
+                hvp += _hvp.detach()
+
+            itr += 1
+            if itr == 10:
+                break
+
+        # if preconditioner is not None, we need to compute P^(-1/2) * hv
+        if preconditioner is not None:
+            hvp = hvp / preconditioner.sqrt()
+
+        return hvp
+
+    def __call__(self, model, loss_fn, train_dataset, preconditioner=None):
+        """
+        Runs the Lanczos algorithm to get extreme eigenvalues of the Hessian.
+        https://iclr-blogposts.github.io/2024/blog/bench-hvp/
+        """
+        params = list(model.parameters())
+        num_params = sum(p.numel() for p in params)
+        device = params[0].device
+
+        # variables
+        q_vectors = []
+        alpha_list = []
+        beta_list = []
+
+        # Initialize the first Lanczos vector (normalized random vector)
+        v = torch.randn(num_params, device=device)
+        v = v / torch.norm(v)
+
+        if self.init_vector is None:
+            self.init_vector = v
+
+        # initialize the 0-th iteration of Lanczos's algorithm
+        w = self.hvp(model, loss_fn, train_dataset, params, v, preconditioner)
+        alpha = torch.dot(v, w)
+        w = w - alpha * v
+
+        # we start collecting from alpha_0
+        q_vectors.append(v)
+        alpha_list.append(alpha.item())
+
+        for i in range(1, self.num_iterations):
+            beta = torch.norm(w)
+            v = w / beta
+
+            w = self.hvp(model, loss_fn, train_dataset, params, v, preconditioner)
+            alpha = torch.dot(v, w)
+            w = w - alpha * v - beta * q_vectors[i - 1]
+
+            # after accessing q_vectors[i - 1], we add current itrerations v to q_vectors
+            # here we collect alpha_i and beta_i
+            q_vectors.append(v)
+            alpha_list.append(alpha.item())
+            beta_list.append(beta.item())
+
+            # if beta is less than tolerance in the 1st iteration, we need a minimum of 2x2 T matrix
+            if beta < self.tol:
+                break
+
+        # Construct the tridiagonal matrix T
+        T = torch.diag(torch.tensor(alpha_list, device=device))
+        for i in range(len(beta_list)):
+            # above the diagonal
+            T[i, i + 1] = beta_list[i]
+
+            # below the diagonal
+            T[i + 1, i] = beta_list[i]
+
+        # Compute eigenvalues of T
+        eigenvalues, eigenvectors = torch.linalg.eigh(T)
+
+        # Recover Ritz vectors from Q and eigenvectors of T
+        Q = torch.stack(q_vectors, dim=1)
+        largest_index = torch.argmax(eigenvalues)
+        smallest_index = torch.argmin(eigenvalues)
+
+        largest_eigenvalue = eigenvalues[largest_index]
+        smallest_eigenvalue = eigenvalues[smallest_index]
+
+        largest_ritz_vector = Q @ eigenvectors[:, largest_index]
+        smallest_ritz_vector = Q @ eigenvectors[:, smallest_index]
+
+        # get the eigenvalue spectrum density of the Hessian
+        eigen_list = eigenvalues.tolist()
+        weight_list = torch.pow(eigenvectors[0,:], 2).tolist()
+
+        # TODO: angle between grad, ritz vectors, update vector
+
+        return eigen_list, weight_list
+
+
+class LanczosAlgorithm:
+    def __init__(self, num_iterations=20, tol=1e-6, init_vector=None):
         self.num_iterations = num_iterations
         self.tol = tol
 
         self.loss = None
         self.grad_params = None
+        self.init_vector = init_vector
 
     def hvp(self, loss, params, v, preconditioner=None):
         if self.grad_params is None:
@@ -114,12 +243,15 @@ class LanczosAlgorithm:
         # Forward pass and compute loss, but dont call loss.backward()
         if self.loss is None:
             outputs = model(data)
-            loss = loss_fn(outputs, target)
+            loss = loss_fn(outputs, target) / data.shape[0]
             self.loss = loss
 
         # Initialize the first Lanczos vector (normalized random vector)
-        v = torch.randn(num_params, device=device)
-        v = v / torch.norm(v)
+        if self.init_vector is not None:
+            v = self.init_vector
+        else:
+            v = torch.randn(num_params, device=device)
+            v = v / torch.norm(v)
 
         # initialize the 0-th iteration of Lanczos's algorithm
         w = self.hvp(self.loss, params, v, preconditioner)
@@ -180,82 +312,6 @@ class LanczosAlgorithm:
         return eigen_list, weight_list
 
 
-class ArnoldiIteration:
-    def __init__(self, n_iterations=10, tol=1e-6):
-        self.n_iterations = n_iterations
-        self.tol = tol
-
-        # cache model forward and grad
-        self.loss = None
-        self.outputs = None
-        self.grad_params = None
-
-    def hvp(self, loss, params, v):
-        """Compute Hessian-vector product"""
-        # cache first order grad computation
-        if self.grad_params is None:
-            grad_params = torch.autograd.grad(loss, params, create_graph=True)
-            self.grad_params = torch.cat([g.flatten() for g in grad_params])
-
-        grad_v_prod = torch.sum(self.grad_params * v)
-        hvp = torch.autograd.grad(grad_v_prod, params, retain_graph=True)
-
-        return torch.cat([g.flatten() for g in hvp])
-
-    def __call__(self, model, loss_fn, batch_data, batch_labels):
-        """
-            because of the Arnoldi iteration, we run only a single iteration of stochastic Lanczos
-        """
-        # Get model parameters
-        params = list(model.parameters())
-        n_params = sum(p.numel() for p in params)
-
-        # cache forward pass and loss computation
-        self.outputs = model(batch_data)
-        self.loss = loss_fn(self.outputs, batch_labels)
-
-        # Initialize first basis vector randomly
-        q1 = torch.randn(n_params, device=params[0].device)
-        q1 = q1 / torch.norm(q1)
-
-        # Initialize matrices for Arnoldi iteration
-        Q = torch.zeros(n_params, self.n_iterations + 1, device=params[0].device)
-        H = torch.zeros(self.n_iterations + 1, self.n_iterations, device=params[0].device)
-        Q[:, 0] = q1
-
-        # Run Arnoldi iteration
-        early_stop = False
-        for k in range(self.n_iterations):
-            # Compute Hessian-vector product
-            w = self.hvp(self.loss, params, Q[:, k])
-
-            # Arnoldi iteration step
-            for j in range(k + 1):
-                H[j, k] = torch.dot(Q[:, j], w)
-                w = w - H[j, k] * Q[:, j]
-
-            H[k + 1, k] = torch.norm(w)
-            if H[k + 1, k] < self.tol:
-                H = H[:k + 1, :k + 1]
-                Q = Q[:, :k + 1]
-
-                early_stop = True
-                break
-
-            Q[:, k + 1] = w / H[k + 1, k]
-
-        # Compute eigenvalues and eigenvectors of H
-        # Convert Ritz vectors back to original space
-        if early_stop:
-            eigenvalues, eigenvectors = torch.linalg.eigh(H)
-            # ritz_vectors = torch.mm(Q, eigenvectors)
-        else:
-            eigenvalues, eigenvectors = torch.linalg.eigh(H[:-1, :])
-            # ritz_vectors = torch.mm(Q[:, :-1], eigenvectors)
-
-        return eigenvalues  # , ritz_vectors
-
-
 class AdamW(Optimizer):
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
@@ -291,12 +347,6 @@ class AdamW(Optimizer):
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
         """
-        # del self._param_tensors
-        # del self._grad_tensors
-
-        # self._param_tensors = []
-        # self._grad_tensors = []
-
         loss = None
         if closure is not None:
             with torch.enable_grad():
@@ -304,9 +354,6 @@ class AdamW(Optimizer):
 
         for group in self.param_groups:
             for p in group['params']:
-                # self._param_tensors.extend([p])
-                # self._grad_tensors.extend([p.grad])
-
                 if p.grad is None:
                     continue
 
@@ -358,69 +405,6 @@ class AdamW(Optimizer):
 
         return loss
 
-    # def post_step_metrics(self):
-    #     _param_tensors = nn.utils.parameters_to_vector(self._param_tensors)
-    #     _grad_tensors = nn.utils.parameters_to_vector(self._grad_tensors)
-
-    #     eigenvalues = arnoldi_iteration_hvp(_param_tensors, _grad_tensors, k=50).real
-
-    #     return eigenvalues
-
-
-# def hessian_vector_product(func, inputs, v):
-#     return hvp(func, inputs, v, create_graph=True)
-
-
-# def arnoldi_iteration_hvp(loss_fn, inputs, params, k):
-#     """
-#     Perform the Arnoldi iteration to approximate eigenvalues using Hessian-vector products.
-    
-#     Parameters:
-#     - params: NN parameters.
-#     - grad: first order gradient of model parameters from backprop.
-#     - k: Integer, the number of Arnoldi iterations to perform.
-    
-#     Returns:
-#     - eigenvalues: torch.Tensor of approximated eigenvalues.
-#     """
-#     n = params.numel()
-#     Q = torch.zeros((n, k + 1)).to(params.device)
-#     H = torch.zeros((k + 1, k)).to(params.device)
-
-#     # embedding layer will have NaN grads
-#     grad = torch.nan_to_num(grad, nan=0.)
-    
-#     # Start with a random unit vector
-#     q = torch.randn(n).to(params.device)
-#     q /= torch.norm(q)
-#     Q[:, 0] = q
-    
-#     for j in range(k):
-#         # Compute Hessian-vector product
-#         # v = torch.autograd.grad(grad, params, grad_outputs=Q[:, j], retain_graph=True)
-#         v = hessian_vector_product(loss_fn, inputs, Q[:, j])
-#         v = torch.nan_to_num(v, nan=0.)
-#         # v = hessian_vector_product(params, grad, Q[:, j])
-
-#         # Modified Gram-Schmidt orthogonalization
-#         for i in range(j + 1):
-#             H[i, j] = torch.dot(Q[:, i], v)
-#             v -= H[i, j] * Q[:, i]
-        
-#         H[j + 1, j] = torch.norm(v)
-#         if H[j + 1, j] > 1e-10:
-#             Q[:, j + 1] = v / H[j + 1, j]
-#         else:
-#             # Convergence achieved
-#             break
-    
-#     # Truncate H to the correct size
-#     H_reduced = H[:j + 1, :j + 1]
-#     # Compute eigenvalues of the Hessenberg matrix H
-#     eigenvalues = torch.linalg.eigvals(H_reduced)
-
-#     return eigenvalues
-
 
 def get_adam_nu(optimizer) -> torch.Tensor:
     vec = []
@@ -459,80 +443,36 @@ def main(dataset: str, arch_id: str, loss: str, opt: str,
     iterates = torch.zeros(max_steps // iterate_freq if iterate_freq > 0 else 0, len(projectors))
     eigs = torch.zeros(max_steps // eig_freq if eig_freq >= 0 else 0, neigs)
 
-    itr = 0
     for step in range(0, max_steps):
-        # train_loss[step], train_acc[step] = compute_losses(network, [loss_fn, acc_fn], train_dataset,
-        #                                                    physical_batch_size)
-        # test_loss[step], test_acc[step] = compute_losses(network, [loss_fn, acc_fn], test_dataset, physical_batch_size)
-
-        # # at step = 0, Adam optimizer has no state, so don't record eigs then        
-        # if step > 0 and eig_freq != -1 and step % eig_freq == 0:
-        #     nu = get_adam_nu(optimizer)
-        #     P = (1 - beta1**step) * ((nu / (1 - beta2**step)).sqrt() + epsilon)
-        #     eigs[step // eig_freq, :] = get_hessian_eigenvalues(network, loss_fn, abridged_train, neigs=neigs,
-        #                                                         physical_batch_size=physical_batch_size, P=P)
-        #     print("eigenvalues: ", eigs[step//eig_freq, :])
-
-        # if iterate_freq != -1 and step % iterate_freq == 0:
-        #     iterates[step // iterate_freq, :] = projectors.mv(parameters_to_vector(network.parameters()).cpu().detach())
-
-        # if save_freq != -1 and step % save_freq == 0:
-        #     save_files(directory, [("eigs", eigs[:step // eig_freq]), ("iterates", iterates[:step // iterate_freq]),
-        #                            ("train_loss", train_loss[:step]), ("test_loss", test_loss[:step]),
-        #                            ("train_acc", train_acc[:step]), ("test_acc", test_acc[:step])])
-
-        # print(f"{step}\t{train_loss[step]:.3f}\t{train_acc[step]:.3f}\t{test_loss[step]:.3f}\t{test_acc[step]:.3f}")
-
-        # if (loss_goal != None and train_loss[step] < loss_goal) or (acc_goal != None and train_acc[step] > acc_goal):
-        #     break
-
-        # optimizer
-
         optimizer.zero_grad()
 
+        lanczos = MicrobatchLanczos(num_iterations=20)
+        eigen_list1, weight_list1 = lanczos(network, loss_fn, train_dataset)
+        init_vector = lanczos.init_vector
+
+        # don't use micro-batch to match computed eigenvalues
+        lanczos = LanczosAlgorithm(num_iterations=20, init_vector=init_vector)
+        itr = 0
+        # micro-batching begins
+        for (X, y) in iterate_dataset(train_dataset, 1000):
+            # TODO: lanczos
+            eigen_list2, weight_list2 = lanczos(network, loss_fn, X, y)
+            itr += 1
+
+            if itr == 1:
+                break
+
+        # TODO: assert grads and Hessians computed over 10 batches vs for 1 batch is the same
+        import ipdb
+        ipdb.set_trace()
+
+
+        # micro-batching for grad updates
         for (X, y) in iterate_dataset(train_dataset, physical_batch_size):
-            # arnoldi = ArnoldiIteration(n_iterations=100, tol=1e-6)
-            # a_eigenvals = arnoldi(network, loss_fn, X, y)
-
-
-            # TODO: in the 0-th step this will fail, but step-1, get optimizer preconditioner here from last step
-            # compute preconditioner P from optimizer state and send it
-            if itr > 0:
-                nu = get_adam_nu(optimizer)
-                step = itr
-                preconditioner = (1 - beta1**step) * ((nu / (1 - beta2**step)).sqrt() + epsilon)
-                preconditioner = preconditioner.cuda()
-            else:
-                preconditioner = None
-
-            lanczos = LanczosAlgorithm(num_iterations=50)
+            
             eigen_list, weight_list = lanczos(network, loss_fn, X, y, preconditioner)
             get_esd_plot([eigen_list], [weight_list], itr)
 
-            # a_eigenvals = torch.sort(a_eigenvals)[0]
-            # l_eigenvals = torch.sort(l_eigenvals)[0]
-
-            # try:
-            #     assert abs(a_eigenvals[-1].item() - l_eigenvals[-1].item()) < 1
-            #     assert abs(a_eigenvals[0].item() - l_eigenvals[0].item()) < 1
-            # except AssertionError:
-            #     print('------------------------------------------------------------')
-            #     print(f"Arnoldi eigenvalue: {a_eigenvals[-1].item()}, {a_eigenvals[0].item()}")
-            #     print(f"Lanczos eigenvalue: {l_eigenvals[-1].item()}, {l_eigenvals[0].item()}")
-            #     print('------------------------------------------------------------')
-
-            # import ipdb; ipdb.set_trace()
-
-            # hessian_comp = hessian(network, loss_fn, data=(X, y), cuda=True)
-            # top_eigenvalues, top_eigenvector = hessian_comp.eigenvalues(maxIter=50, tol=1e-6, top_n=10)
-
-            # try:
-            #     assert abs(torch.sort(eigenvalues)[0][-1].item() - top_eigenvalues[0]) < 10
-            # except AssertionError:
-            #     print('------------------------------------------------------------')
-            #     print(f"Arnoldi eigenvalue: {torch.sort(eigenvalues)[0][-1].item()}")
-            #     print(f"Power iteration eigenvalue: {top_eigenvalues[0]}")
-            #     print('------------------------------------------------------------')
 
             loss = loss_fn(network(X.cuda()), y.cuda()) / len(train_dataset)
             loss.backward()
@@ -540,7 +480,6 @@ def main(dataset: str, arch_id: str, loss: str, opt: str,
             print(f"Step {step}, Loss: {loss.item()}")
 
             optimizer.step()
-            itr += 1
 
     save_files_final(directory,
                      [("eigs", eigs[:(step + 1) // eig_freq]), ("iterates", iterates[:(step + 1) // iterate_freq]),
--- src/arnoldi.py
+++ src/arnoldi.py
@@ -74,19 +74,63 @@ class ArnoldiIteration:
         return eigenvalues  # , ritz_vectors
 
 
-class ArnoldiIterationDensitySpectrum:
-    def __init__(self, n_iterations=10, tol=1e-6):
-        self.n_iterations = n_iterations
+def get_esd_plot(eigenvalues, weights, itr):
+    density, grids = density_generate(eigenvalues, weights)
+    plt.semilogy(grids, density + 1.0e-7)
+    plt.ylabel('Density (Log Scale)', fontsize=14, labelpad=10)
+    plt.xlabel('Eigenvlaue', fontsize=14, labelpad=10)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.axis([np.min(eigenvalues) - 1, np.max(eigenvalues) + 1, None, None])
+    plt.tight_layout()
+    plt.savefig(f"plots/{itr}.png")
+    plt.close()
+    plt.clf()
+
+
+def density_generate(eigenvalues,
+                     weights,
+                     num_bins=10000,
+                     sigma_squared=1e-5,
+                     overhead=0.01):
+
+    eigenvalues = np.array(eigenvalues)
+    weights = np.array(weights)
+
+    lambda_max = np.mean(np.max(eigenvalues, axis=1), axis=0) + overhead
+    lambda_min = np.mean(np.min(eigenvalues, axis=1), axis=0) - overhead
+
+    grids = np.linspace(lambda_min, lambda_max, num=num_bins)
+    sigma = sigma_squared * max(1, (lambda_max - lambda_min))
+
+    num_runs = eigenvalues.shape[0]
+    density_output = np.zeros((num_runs, num_bins))
+
+    for i in range(num_runs):
+        for j in range(num_bins):
+            x = grids[j]
+            tmp_result = gaussian(eigenvalues[i, :], x, sigma)
+            density_output[i, j] = np.sum(tmp_result * weights[i, :])
+    density = np.mean(density_output, axis=0)
+    normalization = np.sum(density) * (grids[1] - grids[0])
+    density = density / normalization
+    return density, grids
+
+
+def gaussian(x, x0, sigma_squared):
+    return np.exp(-(x0 - x)**2 /
+                  (2.0 * sigma_squared)) / np.sqrt(2 * np.pi * sigma_squared)
+
+
+class LanczosAlgorithm:
+    def __init__(self, num_iterations=20, tol=1e-6):
+        self.num_iterations = num_iterations
         self.tol = tol
 
-        # cache model forward and grad
         self.loss = None
-        self.outputs = None
         self.grad_params = None
 
     def hvp(self, loss, params, v):
-        """Compute Hessian-vector product"""
-        # cache first order grad computation
         if self.grad_params is None:
             grad_params = torch.autograd.grad(loss, params, create_graph=True)
             self.grad_params = torch.cat([g.flatten() for g in grad_params])
@@ -96,117 +140,84 @@ class ArnoldiIterationDensitySpectrum:
 
         return torch.cat([g.flatten() for g in hvp])
 
-    def __call__(self, model, loss_fn, batch_data, batch_labels):
+    def __call__(self, model, loss_fn, data, target):
         """
-            because of the Arnoldi iteration, we run only a single iteration of stochastic Lanczos
+        Runs the Lanczos algorithm to get extreme eigenvalues of the Hessian.
+        https://iclr-blogposts.github.io/2024/blog/bench-hvp/
         """
-        # Get model parameters
         params = list(model.parameters())
-        n_params = sum(p.numel() for p in params)
+        num_params = sum(p.numel() for p in params)
+        device = params[0].device
 
-        # cache forward pass and loss computation
-        self.outputs = model(batch_data)
-        self.loss = loss_fn(self.outputs, batch_labels)
+        # variables
+        q_vectors = []
+        alpha_list = []
+        beta_list = []
 
-        ### temp soln for eigen density stuff
-        from copy import deepcopy
-        model2 = deepcopy(model)
-        model2.train()
+        # Forward pass and compute loss
+        outputs = model(data)
+        loss = loss_fn(outputs, target)
+        self.loss = loss
 
-        output2 = model2(batch_data)
-        loss2 = loss_fn(output2, batch_labels)
-        loss2.backward(create_graph=True)
+        # Initialize the first Lanczos vector (normalized random vector)
+        v = torch.randn(num_params, device=device)
+        v = v / torch.norm(v)
 
-        ### generate eigen density plot
-        self.params, self.gradsH = get_params_grad(model2)
-        _v = [torch.randint_like(p, high=2, device=params[-1].device) for p in self.params]
-        # generate Rademacher random variables
-        for v_i in _v:
-            v_i[v_i == 0] = -1
-        _v = normalization(_v)
+        # initialize the 0-th iteration of Lanczos's algorithm
+        w = self.hvp(self.loss, params, v)
+        alpha = torch.dot(v, w)
+        w = w - alpha * v
 
-        # standard lanczos algorithm initlization
-        _v_list = [_v]
-        _w_list = []
-        _alpha_list = []
-        _beta_list = []
+        # we start collecting from alpha_0
+        q_vectors.append(v)
+        alpha_list.append(alpha.item())
 
-        # Initialize first basis vector randomly
-        q1 = torch.randn(n_params, device=params[0].device)
-        q1 = q1 / torch.norm(q1)
+        for i in range(1, self.num_iterations):
+            beta = torch.norm(w)
+            v = w / beta
 
-        # Initialize matrices for Arnoldi iteration
-        Q = torch.zeros(n_params, self.n_iterations + 1, device=params[0].device)
-        H = torch.zeros(self.n_iterations + 1, self.n_iterations, device=params[0].device)
-        Q[:, 0] = q1
+            w = self.hvp(self.loss, params, v)
+            alpha = torch.dot(v, w)
+            w = w - alpha * v - beta * q_vectors[i - 1]
 
-        # Run Arnoldi iteration
-        early_stop = False
-        for k in range(self.n_iterations):
-            # Compute Hessian-vector product
-            w = self.hvp(self.loss, params, Q[:, k])
+            # after accessing q_vectors[i - 1], we add current itrerations v to q_vectors
+            # here we collect alpha_i and beta_i
+            q_vectors.append(v)
+            alpha_list.append(alpha.item())
+            beta_list.append(beta.item())
 
-            # Arnoldi iteration step
-            for j in range(k + 1):
-                H[j, k] = torch.dot(Q[:, j], w)
-                w = w - H[j, k] * Q[:, j]
-
-            H[k + 1, k] = torch.norm(w)
-            if H[k + 1, k] < self.tol:
-                H = H[:k + 1, :k + 1]
-                Q = Q[:, :k + 1]
-
-                early_stop = True
+            # if beta is less than tolerance in the 1st iteration, we need a minimum of 2x2 T matrix
+            if beta < self.tol:
                 break
 
-            Q[:, k + 1] = w / H[k + 1, k]
+        # Construct the tridiagonal matrix T
+        T = torch.diag(torch.tensor(alpha_list, device=device))
+        for i in range(len(beta_list)):
+            # above the diagonal
+            T[i, i + 1] = beta_list[i]
 
-            ### code for eigen density part
-            if k == 0:
-                _w_prime = hessian_vector_product(self.gradsH, self.params, _v)
-                _alpha = group_product(_w_prime, _v)
-                _alpha_list.append(_alpha.cpu().item())
-                _w = group_add(_w_prime, _v, alpha=-_alpha)
-                _w_list.append(_w)
-            else:
-                _beta = torch.sqrt(group_product(_w, _w))
-                _beta_list.append(_beta.cpu().item())
-                if _beta_list[-1] != 0.:
-                    # We should re-orth it
-                    _v = orthnormal(_w, _v_list)
-                    _v_list.append(_v)
-                else:
-                    # generate a new vector
-                    _w = [torch.randn(p.size()).to(device) for p in self.params]
-                    _v = orthnormal(_w, _v_list)
-                    _v_list.append(_v)
+            # below the diagonal
+            T[i + 1, i] = beta_list[i]
 
-                _w_prime = hessian_vector_product(self.gradsH, self.params, _v)
-                _alpha = group_product(_w_prime, _v)
-                _alpha_list.append(_alpha.cpu().item())
+        # Compute eigenvalues of T
+        eigenvalues, eigenvectors = torch.linalg.eigh(T)
 
-                _w_tmp = group_add(_w_prime, _v, alpha=-_alpha)
-                _w = group_add(_w_tmp, _v_list[-2], alpha=-_beta)
+        # Recover Ritz vectors from Q and eigenvectors of T
+        Q = torch.stack(q_vectors, dim=1)
+        largest_index = torch.argmax(eigenvalues)
+        smallest_index = torch.argmin(eigenvalues)
 
-        ### code for eigen density part
-        _T = torch.zeros(self.n_iterations, self.n_iterations).to(params[-1].device)
-        for i in range(len(_alpha_list)):
-            _T[i, i] = _alpha_list[i]
-            if i < len(_alpha_list) - 1:
-                _T[i + 1, i] = _beta_list[i]
-                _T[i, i + 1] = _beta_list[i]
-        _eigenvalues, _eigenvectors = torch.linalg.eig(_T)
+        largest_eigenvalue = eigenvalues[largest_index]
+        smallest_eigenvalue = eigenvalues[smallest_index]
 
-        _eigen_list = list(_eigenvalues.real.cpu().numpy())
-        _weight_list = list(torch.pow(_eigenvectors[0,:], 2).cpu().numpy())
+        largest_ritz_vector = Q @ eigenvectors[:, largest_index]
+        smallest_ritz_vector = Q @ eigenvectors[:, smallest_index]
 
-        # Compute eigenvalues and eigenvectors of H
-        # Convert Ritz vectors back to original space
-        if early_stop:
-            eigenvalues, eigenvectors = torch.linalg.eigh(H)
-            # ritz_vectors = torch.mm(Q, eigenvectors)
-        else:
-            eigenvalues, eigenvectors = torch.linalg.eigh(H[:-1, :])
-            # ritz_vectors = torch.mm(Q[:, :-1], eigenvectors)
+        # get the eigenvalue spectrum density of the Hessian
+        eigen_list = eigenvalues.tolist()
+        weight_list = torch.pow(eigenvectors[0,:], 2).tolist()
 
-        return eigenvalues  # , ritz_vectors
+        # TODO: get eigenvalues according to preconditioner P^(-1)H
+        # TODO: angle between grad, ritz vectors, update vector
+
+        return eigen_list, weight_list
